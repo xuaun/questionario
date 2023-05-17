45d704b7c5934a3b68862fb2d91a2fe9982eca37
João Víctor de Araujo Lima <joaovictor.ne@gmail.com>
2023-05-17 11:18:50 -0300

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -2,9 +2,13 @@ function SurveyComponent() {
     const survey = new Survey.Model(json);
     survey.onComplete.add((sender, options) => {
         const data = sender.data;
+        console.log(data);
+
+        const fields = ['Nome', 'Idade', 'P1', 'P2', 'P3', 'P4', 'P5', 'P6', 'P7', 'P8', 'P9', 'P10', 'P11', 'P12', 'P13', 'P14', 'P15'];
+
         const element = document.createElement("a");
 
-        element.setAttribute("href", `data:text/csv;charset=utf-8,${json2csv.parse(data)}`);
+        element.setAttribute("href", `data:text/csv;charset=utf-8,${json2csv.parse(data,{fields})}`);
         element.setAttribute("download", "filename.csv");
         element.style.display = "none";
 
